9d8a3a4eb82323a9e76360e55857c54c1a6604fd
IamShahzaibAli-SA <243137271+IamShahzaibAli-SA@users.noreply.github.com>
2025-11-23 15:16:00 +0500

    Color API BUILD

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const fetchAPI = async () =>{
+    const Url = "https://x-colors.yurace.pro/api/random";
+    const res = await fetch(Url);
+    const result = await res.json();
+
+    console.log(result);
+
+    setCount(result.hex);
+  }
+
+  useEffect(()=>{
+    fetchAPI();
+  }, []);
 
   return (
     <>
-      
+      <div className='flex justify-center'>
+        <div className="w-[1200px] h-[800px]" style={{backgroundColor: count }}></div>
+      </div>
     </>
   )
 }
